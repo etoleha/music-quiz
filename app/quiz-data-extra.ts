c7bd8efd4d9888d5c86b595d8d64b1f4601f392c
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2026-09-12 11:37:09 +0400

    Publish daily music quiz XXXIV

--- app/quiz-data-extra.ts
+++ app/quiz-data-extra.ts
@@ -13,6 +13,7 @@ import verifiedRelease11 from "../data/quiz-release-new-rules-11.json";
 import verifiedRelease12 from "../data/quiz-release-new-rules-12.json";
 import verifiedRelease13 from "../data/quiz-release-new-rules-13.json";
 import verifiedRelease14 from "../data/quiz-release-new-rules-14.json";
+import verifiedRelease15 from "../data/quiz-release-new-rules-15.json";
 
 const performerForms: Partial<Record<string, Track["artistForm"]>> = {
   "Александр Барыкин": "Исполнитель",
@@ -478,7 +479,7 @@ const extraTrack = (youtubeId: string, artist: string, title: string, start: num
   };
 };
 
-type VerifiedReleaseTrack = (typeof verifiedRelease.tracks)[number] | (typeof verifiedRelease02.tracks)[number] | (typeof verifiedRelease03.tracks)[number] | (typeof verifiedRelease04.tracks)[number] | (typeof verifiedRelease05.tracks)[number] | (typeof verifiedRelease06.tracks)[number] | (typeof verifiedRelease07.tracks)[number] | (typeof verifiedRelease08.tracks)[number] | (typeof verifiedRelease09.tracks)[number] | (typeof verifiedRelease10.tracks)[number] | (typeof verifiedRelease11.tracks)[number] | (typeof verifiedRelease12.tracks)[number] | (typeof verifiedRelease13.tracks)[number] | (typeof verifiedRelease14.tracks)[number];
+type VerifiedReleaseTrack = (typeof verifiedRelease.tracks)[number] | (typeof verifiedRelease02.tracks)[number] | (typeof verifiedRelease03.tracks)[number] | (typeof verifiedRelease04.tracks)[number] | (typeof verifiedRelease05.tracks)[number] | (typeof verifiedRelease06.tracks)[number] | (typeof verifiedRelease07.tracks)[number] | (typeof verifiedRelease08.tracks)[number] | (typeof verifiedRelease09.tracks)[number] | (typeof verifiedRelease10.tracks)[number] | (typeof verifiedRelease11.tracks)[number] | (typeof verifiedRelease12.tracks)[number] | (typeof verifiedRelease13.tracks)[number] | (typeof verifiedRelease14.tracks)[number] | (typeof verifiedRelease15.tracks)[number];
 const verifiedReleaseTrack = (song: VerifiedReleaseTrack): Track => ({
   key: `${song.artist}—${song.title}`.toLocaleLowerCase("ru-RU"),
   youtubeId: song.youtube.videoId,
@@ -503,6 +504,13 @@ const verifiedReleaseTrack = (song: VerifiedReleaseTrack): Track => ({
 });
 
 export const extraQuizzes: Quiz[] = [
+  {
+    id: verifiedRelease15.quiz.id,
+    title: verifiedRelease15.quiz.title,
+    level: verifiedRelease15.quiz.level,
+    published: verifiedRelease15.quiz.published,
+    tracks: verifiedRelease15.tracks.map(verifiedReleaseTrack),
+  },
   {
     id: verifiedRelease14.quiz.id,
     title: verifiedRelease14.quiz.title,
